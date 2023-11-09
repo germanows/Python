821b289a66537ebc0f10d53585ec69f7a3355ca0
gege <germano2227@gmail.com>
2023-11-09 14:39:46 -0300

    bisnaga

--- forca.py
+++ forca.py
@@ -1,5 +1,41 @@
-print("*********************************")
-print("***Bem vindo ao jogo de Forca!***")
-print("*********************************")
+def jogar():
 
-print("Fim do jogo")
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print("\n*********************************")
+    print("***Bem vindo ao jogo de Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_","_","_","_","_","_"]
+
+    print(letras_acertadas)
+
+    while (not enforcou and not acertou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    print(chute)
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas                
+        print(letras_acertadas)
+    if(acertou):
+        print("boa filha da puta!!!!!!!!!!!!!!")
+    else:
+        print("errou feio seu burro kkkkkkkkk") 
+    print("Fim do Jogo")    
+
+
+if(__name__ == "__main__"):
+    jogar()
--- jogos.py
+++ jogos.py
@@ -1,5 +1,18 @@
+import forca
+import projetinho
+
 print("*********************************")
 print("******Escolha o seu Jogo!********")
 print("*********************************")
 
-print("Fim do jogo")
+print("(1) - Forca (2) - Adivinhação")
+
+jogo = int(input("Qual jogo?"))
+
+if (jogo == 1):
+    print("\nJogando Forquinha\n")
+    forca.jogar()
+elif(jogo == 2):
+    print("\nJogando adivinhaçãozinha\n")
+    projetinho.jogar()
+
