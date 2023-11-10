392c43b66243d6f56b064184d92a1102adb98fc9
gege <germano2227@gmail.com>
2023-11-10 13:28:26 -0300

    +1

--- forca.py
+++ forca.py
@@ -7,29 +7,20 @@ def inicializa_letras(palavra_secreta):
 def jogar():
 
     mensagem_abertura()
-
-    vagabunda()
-
     palavra_secreta = vagabunda()
-    
     letras_acertadas = inicializa_letras(palavra_secreta)
+    print(letras_acertadas)
 
     enforcou = False
     acertou = False
     erros = 0
     
-    print(letras_acertadas)
-
     while (not enforcou and not acertou):
 
-        chute = input("Qual a letra? ")
-        chute = chute.strip().upper()
+        chute = bicuda()
+
         if(chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if(chute == letra):
-                    letras_acertadas[index] = letra
-                index += 1
+            bicuda_certeira(palavra_secreta, chute, letras_acertadas)
         else:
             erros += 1
 
@@ -39,8 +30,7 @@ def jogar():
     if(acertou):
         print("boa filha da puta!!!!!!!!!!!!!!")
     else:
-        print("errou feio seu burro kkkkkkkkk") 
-    print("Fim do Jogo")    
+        print("errou feio seu burro kkkkkkkkk")   
 
 
 if(__name__ == "__main__"):
@@ -53,6 +43,7 @@ def mensagem_abertura():
     print("*********************************")
 
 def vagabunda():
+
     arquivo = open("palavras.txt", "r")
     palavras = []
 
@@ -65,3 +56,15 @@ def vagabunda():
     rando = random.randrange(0,len(palavras))
     palavra_secreta = palavras[rando].upper()
     return palavra_secreta
+
+def bicuda():
+    chute = input("Qual a letra? ")
+    chute = chute.strip().upper()
+    return bicuda
+
+def bicuda_certeira(palavra_secreta, chute, letras_acertadas):
+    index = 0
+    for letra in palavra_secreta:
+        if(chute == letra):
+            letras_acertadas[index] = letra
+            index += 1
